669c855b99b177d351b261f6b00c68fc690d8870
RownieMatt <basubasrowniematthew@gmail.com>
2023-02-27 19:26:40 -0700

    Demo7 Updated

--- CSharpPractice/Model/Demo7ArithmeticPractice.cs
+++ CSharpPractice/Model/Demo7ArithmeticPractice.cs
@@ -81,5 +81,25 @@ namespace DemoForReal.Model
             Console.WriteLine(Result);
 
         }
+        public static void FCE(int num)
+        {
+            int[] Array1 = { 1, 2, 3 };
+            int[] Array2 = { 3, 4, 5 };
+            int[] Array3 = { 5, 3, 7 };
+            for (int A1 = 0; A1 < Array1.Length; A1++)
+            {
+                for (int A2 = 0; A2 < Array2.Length; A2++)
+                {
+                    for (int A3 = 0; A3 < Array3.Length; A3++)
+                    {
+                        if (Array1[A1] == num && Array2[A2] == num && Array3[A3] == num)
+                        {
+                            Console.WriteLine($"All 3 Arrays have {num} in them");
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
