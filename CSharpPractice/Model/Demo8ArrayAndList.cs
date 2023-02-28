62781823b1a5052ccb199aa8a0b69df1b42baece
RownieMatt <basubasrowniematthew@gmail.com>
2023-02-27 20:23:59 -0700

    Demo 8 Updated

--- CSharpPractice/Model/Demo8ArrayAndList.cs
+++ CSharpPractice/Model/Demo8ArrayAndList.cs
@@ -9,4 +9,41 @@ namespace DemoForReal.Model
     internal class Demo8ArrayAndList
     {
     }
+    public static void ArrayCommonN(int num)
+    {
+        int[] Array1 = { 1, 2, 3 };
+        int[] Array2 = { 3, 4, 5 };
+        int[] Array3 = { 5, 3, 7 };
+        for (int A1 = 0; A1 < Array1.Length; A1++)
+        {
+            for (int A2 = 0; A2 < Array2.Length; A2++)
+            {
+                for (int A3 = 0; A3 < Array3.Length; A3++)
+                {
+                    if (Array1[A1] == num && Array2[A2] == num && Array3[A3] == num)
+                    {
+                        Console.WriteLine($"All 3 Arrays have {num} in them");
+                    }
+                }
+            }
+        }
+
+    }
+    public static void ArraySort(int[] IArray)
+    {
+        Array.Sort(IArray);
+        Console.WriteLine("In Ascending Order:");
+        foreach (int x in IArray)
+        {
+            Console.WriteLine(x);
+        }
+
+        Array.Reverse(IArray);
+        Console.WriteLine("In Descending Order:");
+        foreach (int x in IArray)
+        {
+            Console.WriteLine(x);
+        }
+    }
+
 }
